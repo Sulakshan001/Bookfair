50ca30035e5c155481179a4bf06367852c97118c
Amshavarthana-S <amshavarthana.s@gmail.com>
2026-02-19 14:49:51 +0530

    Add AdminPayment and AdminNotification

--- Frontend/reactapp/src/App.jsx
+++ Frontend/reactapp/src/App.jsx
@@ -14,7 +14,22 @@ import UserAccountApi from './services/UserAccountApi';
 import Authentication from './services/Authentication';
 import { FaSignInAlt } from 'react-icons/fa';
 import Signup from "./Components/pages/Signup";
+<<<<<<< Updated upstream
 
+=======
+import StallMap from './Components/pages/StallMap';
+import AdminDashboard from "./Components/Adminpages/AdminDashboard";
+import AdminStallPricing from './Components/Adminpages/AdminStallPricing';  
+import Adminstalls from './Components/Adminpages/Adminstalls';
+import AdminEvents from './Components/Adminpages/AdminEvents';
+import AdminMaps from './Components/Adminpages/AdminMaps';
+import AdminQrpass from './Components/Adminpages/AdminQrpass';
+import AdminReservertion from './Components/Adminpages/AdminReservertion';
+import Adminstatistics from './Components/Adminpages/Adminstatistic';
+import AdminTransactions from './Components/Adminpages/AdminTransactions';
+import AdminUser from './Components/Adminpages/AdminUser';
+import AdminPayment from './Components/Adminpages/AdminPayment';
+>>>>>>> Stashed changes
 
 function PublicLayout() {
   return (
@@ -43,6 +58,22 @@ function App() {
           <Route path="/sign-in" element={<Signin />} />
           <Route path="/sign-up" element={<Signup />} />
           
+<<<<<<< Updated upstream
+=======
+          <Route index element={<Adminstatistics />} />
+          <Route path="stalls" element={<Adminstalls />} />
+          <Route path="stallPricing" element={<AdminStallPricing />} />
+          <Route path="reservations" element={<AdminReservertion />} />
+          <Route path="events" element={<AdminEvents />} />
+          <Route path="maps" element={<AdminMaps />} />
+          <Route path="qrpasses" element={<AdminQrpass />} />
+          <Route path="transactions" element={<AdminTransactions />} />
+          <Route path="users" element={<AdminUser />} />
+          <Route path="payments" element={<AdminPayment />} />
+          <Route path="notification" element={<AdminNotification />} />
+
+
+>>>>>>> Stashed changes
         </Route>
       </Routes>
     </BrowserRouter>
